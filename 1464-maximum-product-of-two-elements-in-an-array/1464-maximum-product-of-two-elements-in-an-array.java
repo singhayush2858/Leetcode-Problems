01205de5eb32cfdc6919c13df16857955248e25c
singhayush2858 <ayushsingh20082006@gmail.com>
2026-08-16 15:53:37 +0530

    Time: 0 ms (100%), Space: 44.5 MB (72.16%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -7,7 +7,7 @@ class Solution {
                 max2 = max1;
                 max1 = num;
             }
-            else if (num > max2)
+            else if (num > max2 - 1)
                 max2 = num;
         }
         return (max1 - 1) * (max2 - 1);
